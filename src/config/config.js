4dc2413a889bb441a18ea9244467c3aa5ecddb5b
shahzaib <shahzaibshahg90@example.com>
2026-04-24 11:42:31 +0500

    final commit

--- src/config/config.js
+++ src/config/config.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: "https://surplusfood.onrender.com", withCredentials: true });
+const API = axios.create({ baseURL: "https://surplusfood.onrender.com/api/v1", withCredentials: true });
 
 
 // USER APIs........
