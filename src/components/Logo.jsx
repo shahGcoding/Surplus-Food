7944c5035e801f17ec170bc4f2692e6cb93d12da
shahGcoding <sabtainali364@gmail.com>
2026-04-24 12:23:29 +0500

    Change logo image source from food.png to logo.png

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Logo({width = '100px'}) {
   return (
     <img
-      src='/food.png'
+      src='/logo.png'
       alt="Logo"
       className="w-32 h-32 rounded-full"
       style={{ width, height: "auto", display: "block",  }}
