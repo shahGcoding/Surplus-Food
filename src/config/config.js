07c40e6e0b0dd2c961733f0019150196c7328d4e
shahGcoding <sabtainali364@gmail.com>
2026-04-24 11:29:21 +0500

    Update API base URL to include '/api' path

--- src/config/config.js
+++ src/config/config.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: "https://surplusfood.onrender.com", withCredentials: true });
+const API = axios.create({ baseURL: "https://surplusfood.onrender.com/api", withCredentials: true });
 
 // USER APIs........
 
